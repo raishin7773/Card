2ad4ea6bd3d39ee10bed2ad84586162a6579ecfe
raishin <raishin7773@yahoo.co.jp>
2019-07-18 21:02:25 +0900

    logger actuator設定追加

--- src/main/kotlin/com/raishin/search/controller/SearchController.kt
+++ src/main/kotlin/com/raishin/search/controller/SearchController.kt
@@ -3,6 +3,8 @@ package com.raishin.search.controller
 import com.raishin.search.domain.CardsDomain
 import com.raishin.search.form.SearchForm
 import com.raishin.search.service.SearchService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.ModelAttribute
@@ -14,8 +16,11 @@ class SearchController {
     @Autowired
     lateinit var seachService: SearchService
 
+    val logger: Logger = LoggerFactory.getLogger(SearchController::class.java)
+
     @RequestMapping("/")
     fun index(@ModelAttribute searchForm: SearchForm): String {
+        logger.info("start")
         return "index"
     }
 
