62127b24f0e478569afbc923755ed9eb925f129a
raishin7773 <raishin7773@yahoo.co.jp>
2020-12-21 00:07:45 +0900

    デザイン変更。カードタイプ取得。ホットリロードを有効化

--- src/main/kotlin/com/raishin/search/constants/Constants.kt
+++ src/main/kotlin/com/raishin/search/constants/Constants.kt
@@ -3,47 +3,94 @@ package com.raishin.search.constants
 class Constants {
 
     companion object {
-        //モンスター
-        //リバースモンスター
-        const val REVERSE_MONSTER: Long = 2097185
         //効果モンスター
         const val EFFECT_MONSTER: Long = 33
+        //効果モンスター：リバース
+        const val EFFECT_REVERSE_MONSTER: Long = 2097185
+        //効果モンスター：デュアル
+        const val EFFECT_DUAL_MONSTER: Long = 2081
+        //効果モンスター：ユニオン
+        const val EFFECT_UNION_MONSTER: Long = 1057
+        //効果モンスター：ユニオン：チューナー
+        const val EFFECT_UNION_TUNER_MONSTER: Long = 5153
         //効果モンスター：特殊召喚（通常召喚不可）
         const val EFFECT_SS_MONSTER: Long = 33554465
+        //効果モンスター：チューナー：特殊召喚（通常召喚不可）
+        const val EFFECT_TUNER_SS_MONSTER: Long = 33558561
         //効果モンスター：スピリット
         const val SPIRIT_MONSTER: Long = 545
-        //チューナー
+        //効果モンスター：チューナー
         const val EFFECT_TUNER_MONSTER: Long = 4129
+        //儀式：効果モンスター
+        const val GISHKI_EFFECT_MONSTER:Long = 161
+        //儀式：通常モンスター
+        const val GISHKI_NORMAL_MONSTER:Long = 145
         //トークン
         const val TOKEN: Long = 16401
         //通常モンスター
         const val NORMAL_MONSTER: Long = 17
+        //通常モンスター：チューナー
+        const val NORMAL_TUNER_MONSTER: Long = 4113
         //融合通常モンスター
         const val FUJON_NORMAL_MONSTER: Long = 65
         //融合モンスター
         const val FUJON_E_MONSTER: Long = 97
         //ペンデュラムモンスター
         const val PENDURAM_MONSTER: Long = 16777249
+        //ペンデュラムモンスター:チューナー
+        const val PENDURAM_TUNER_MONSTER: Long = 16781345
+        //ペンデュラムモンスター:特殊召喚（通常召喚不可）
+        const val PENDURAM_SS_MONSTER: Long = 50331681
+        //ペンデュラムモンスター：通常
+        const val PENDURAM_NORMAL_MONSTER: Long = 16777233
         //ペンデュラム融合
         const val PENDURAM_FUJON_MONSTER: Long = 16777313
         //ペンデュラムエクシーズ
         const val PENDURAM_XYZ_MONSTER: Long = 25165857
         //ペンデュラムシンクロ
         const val PENDURAM_SYNCHRO_MONSTER: Long = 16785441
-        //シンクロモンスターモンスター
+        //シンクロモンスター
         const val SYNCHRO_MONSTER: Long = 8225
+        //シンクロモンスター：チューナー
+        const val SYNCHRO_TUNER_MONSTER: Long = 12321
+        //シンクロモンスター：通常
+        const val SYNCHRO_NORMAL_MONSTER: Long = 8209
         //エクシーズモンスター
         const val XYZ_MONSTER: Long = 8388641
+        //エクシーズモンスター:通常
+        const val XYZ_NORMAL_MONSTER: Long = 8388625
         //リンクモンスター
         const val LINK_E_MONSTER: Long = 67108897
+        //リンクモンスター：通常
+        const val LINK_NORMAL_MONSTER: Long = 67108865
+
         //モンスターリスト
-        val monsterList = mutableListOf(REVERSE_MONSTER, EFFECT_MONSTER, EFFECT_SS_MONSTER, SPIRIT_MONSTER, EFFECT_TUNER_MONSTER
+        val monsterList = mutableListOf(EFFECT_DUAL_MONSTER,EFFECT_UNION_TUNER_MONSTER,EFFECT_UNION_MONSTER,LINK_NORMAL_MONSTER,EFFECT_TUNER_SS_MONSTER,SYNCHRO_TUNER_MONSTER,PENDURAM_TUNER_MONSTER,PENDURAM_SS_MONSTER,GISHKI_EFFECT_MONSTER,GISHKI_NORMAL_MONSTER,SYNCHRO_NORMAL_MONSTER,XYZ_NORMAL_MONSTER,EFFECT_REVERSE_MONSTER,NORMAL_TUNER_MONSTER, EFFECT_MONSTER, EFFECT_SS_MONSTER, SPIRIT_MONSTER, EFFECT_TUNER_MONSTER
                 , TOKEN, NORMAL_MONSTER, FUJON_NORMAL_MONSTER, FUJON_E_MONSTER, PENDURAM_MONSTER, PENDURAM_FUJON_MONSTER, PENDURAM_XYZ_MONSTER
                 , PENDURAM_SYNCHRO_MONSTER, SYNCHRO_MONSTER, XYZ_MONSTER, LINK_E_MONSTER)
+        //通常モンスターリスト
+        val normalMonsterList = mutableListOf(NORMAL_TUNER_MONSTER,NORMAL_MONSTER,FUJON_NORMAL_MONSTER)
+        //シンクロモンスターリスト
+        val synchroMonsterList = mutableListOf(SYNCHRO_TUNER_MONSTER,SYNCHRO_MONSTER, SYNCHRO_NORMAL_MONSTER)
+        //エクシーズモンスターリスト
+        val xyzMonsterList = mutableListOf(XYZ_MONSTER, XYZ_NORMAL_MONSTER)
+        //儀式モンスターリスト
+        val gishkiMonsterList = mutableListOf(GISHKI_NORMAL_MONSTER, GISHKI_EFFECT_MONSTER)
+        //効果モンスターリスト
+        val effectMonsterList = mutableListOf(EFFECT_DUAL_MONSTER,EFFECT_UNION_TUNER_MONSTER,EFFECT_UNION_MONSTER,EFFECT_TUNER_SS_MONSTER,EFFECT_REVERSE_MONSTER,EFFECT_MONSTER,EFFECT_SS_MONSTER,SPIRIT_MONSTER,EFFECT_TUNER_MONSTER)
+        // ペンでゅらむモンスターリスト
+        val penduramMonsterList = mutableListOf(PENDURAM_TUNER_MONSTER,PENDURAM_SS_MONSTER,PENDURAM_MONSTER)
+        // 融合モンスターリスト
+        val fujonMonsterList = mutableListOf(FUJON_E_MONSTER,FUJON_NORMAL_MONSTER)
+        // リンクモンスターリスト
+        val linkMonsterList = mutableListOf(LINK_E_MONSTER,LINK_NORMAL_MONSTER)
+
 
         //魔法
         //通常魔法
         const val MAGIC: Long = 2
+        //儀式魔法
+        const val GISHKI_MAGIC: Long = 130
         //速攻魔法
         const val SPEED_MAGIC: Long = 65538
         //永続魔法
@@ -53,7 +100,7 @@ class Constants {
         //フィールド魔法
         const val FIELD_MAGIC: Long = 524290
         //魔法リスト
-        val magicList = mutableListOf(MAGIC, SPEED_MAGIC, FOREVER_MAGIC, EQUIP_MAGIC, FIELD_MAGIC)
+        val magicList = mutableListOf(MAGIC, GISHKI_MAGIC,SPEED_MAGIC, FOREVER_MAGIC, EQUIP_MAGIC, FIELD_MAGIC)
 
 
         //罠
--- src/main/kotlin/com/raishin/search/form/SearchForm.kt
+++ src/main/kotlin/com/raishin/search/form/SearchForm.kt
@@ -28,6 +28,9 @@ class SearchForm {
         return mapOf("モンスター" to 1L, "魔法" to 2L, "罠" to 3L)
     }
 
+    /**
+     * カードカテゴリを取得する
+     */
     fun getTypeString(type: Long): String {
         if (Constants.monsterList.contains(type)) {
             return "モンスター"
@@ -41,18 +44,127 @@ class SearchForm {
         return ""
     }
 
+
+    /**
+     * カードタイプの詳細表示名を取得する
+     */
+    fun getCardTypeDetailName(type: Long): String {
+        return when (type) {
+            Constants.EFFECT_MONSTER -> "効果モンスター"
+            Constants.EFFECT_REVERSE_MONSTER -> "効果モンスター：リバース"
+            Constants.EFFECT_DUAL_MONSTER -> "効果モンスター：デュアル"
+            Constants.EFFECT_UNION_MONSTER -> "効果モンスター：ユニオン"
+            Constants.EFFECT_UNION_TUNER_MONSTER -> "効果モンスター：ユニオン：チューナー"
+            Constants.EFFECT_SS_MONSTER -> "効果モンスター：特殊召喚（通常召喚不可）"
+            Constants.EFFECT_TUNER_SS_MONSTER -> "効果モンスター：チューナー：特殊召喚（通常召喚不可）"
+            Constants.SPIRIT_MONSTER -> "効果モンスター：スピリット"
+            Constants.EFFECT_TUNER_MONSTER -> "効果モンスター：チューナー"
+            Constants.GISHKI_EFFECT_MONSTER -> "儀式：効果モンスター"
+            Constants.GISHKI_NORMAL_MONSTER -> "儀式：通常モンスター"
+            Constants.TOKEN -> "トークン"
+            Constants.NORMAL_MONSTER -> "通常モンスター"
+            Constants.NORMAL_TUNER_MONSTER -> "通常モンスター：チューナー"
+            Constants.FUJON_NORMAL_MONSTER -> "融合：通常モンスター"
+            Constants.FUJON_E_MONSTER -> "融合：効果モンスター"
+            Constants.PENDURAM_MONSTER -> "ペンデュラム：効果モンスター"
+            Constants.PENDURAM_TUNER_MONSTER -> "ペンデュラム：効果モンスター：チューナー"
+            Constants.PENDURAM_SS_MONSTER -> "ペンデュラム：効果モンスター：特殊召喚（通常召喚不可）"
+            Constants.PENDURAM_NORMAL_MONSTER -> "ペンデュラム：通常モンスター"
+            Constants.PENDURAM_FUJON_MONSTER -> "ペンデュラム：融合：効果モンスター"
+            Constants.PENDURAM_XYZ_MONSTER -> "ペンデュラム：エクシーズ：効果モンスター"
+            Constants.PENDURAM_SYNCHRO_MONSTER -> "ペンデュラム：シンクロ：効果モンスター"
+            Constants.SYNCHRO_MONSTER -> "シンクロ：効果モンスター"
+            Constants.SYNCHRO_TUNER_MONSTER -> "シンクロ：効果モンスター：チューナー"
+            Constants.SYNCHRO_NORMAL_MONSTER -> "シンクロ：通常モンスター"
+            Constants.XYZ_MONSTER -> "エクシーズ：効果モンスター"
+            Constants.XYZ_NORMAL_MONSTER -> "エクシーズ：通常モンスター"
+            Constants.LINK_E_MONSTER -> "リンク：効果モンスター"
+            Constants.LINK_NORMAL_MONSTER -> "リンク：通常モンスター"
+            Constants.MAGIC -> "通常魔法"
+            Constants.GISHKI_MAGIC -> "儀式魔法"
+            Constants.SPEED_MAGIC -> "速攻魔法"
+            Constants.FOREVER_MAGIC -> "永続魔法"
+            Constants.EQUIP_MAGIC -> "装備魔法"
+            Constants.FIELD_MAGIC -> "フィールド魔法"
+            Constants.TRAP -> "通常罠"
+            Constants.FOREVER_TRAP -> "永続罠"
+            Constants.COUNTER_TRAP -> "カウンター罠"
+            else -> ""
+        }
+    }
+
+    /**
+     * カードタイプから付与したいクラス名を取得する
+     */
+    fun getClass(type: Long): String {
+        if (Constants.effectMonsterList.contains(type)) {
+            return "effect_monster"
+        }
+        if (Constants.normalMonsterList.contains(type)) {
+            return "normal_monster"
+        }
+        if (Constants.synchroMonsterList.contains(type)) {
+            return "synchro_monster"
+        }
+        if (Constants.xyzMonsterList.contains(type)) {
+            return "xyz_monster"
+        }
+        if (Constants.gishkiMonsterList.contains(type)) {
+            return "gishki_monster"
+        }
+        if (Constants.fujonMonsterList.contains(type)) {
+            return "fujon_monster"
+        }
+        if (Constants.penduramMonsterList.contains(type)) {
+            return "penduram_monster"
+        }
+        if (Constants.linkMonsterList.contains(type)) {
+            return "link_monster"
+        }
+        if (Constants.magicList.contains(type)) {
+            return "magic"
+        }
+        if (Constants.trapList.contains(type)) {
+            return "trap"
+        }
+        if (Constants.TOKEN == type) {
+            return "token_monster"
+        }
+        if (Constants.PENDURAM_XYZ_MONSTER == type) {
+            return "penduram_xyz_monster"
+        }
+        if (Constants.PENDURAM_SYNCHRO_MONSTER == type) {
+            return "penduram_synchro_monster"
+        }
+        if (Constants.PENDURAM_FUJON_MONSTER == type) {
+            return "penduram_fujon_monster"
+        }
+        if (Constants.PENDURAM_NORMAL_MONSTER == type) {
+            return "penduram_normal_monster"
+        }
+        return ""
+    }
+
+    /**
+     * 表示する守備力を取得
+     */
     fun getDef(type: Long, def: Long): String {
         if (Constants.LINK_E_MONSTER == type ||
                 Constants.magicList.contains(type) ||
-                Constants.trapList.contains(type)) {
+                Constants.trapList.contains(type) ||
+                def == -2L) {
             return "-"
         }
         return def.toString()
     }
 
+    /**
+     * 表示する攻撃力を取得
+     */
     fun getAtk(type: Long, atk: Long): String {
         if (Constants.magicList.contains(type) ||
-                Constants.trapList.contains(type)) {
+                Constants.trapList.contains(type) ||
+                atk == -2L) {
             return "-"
         }
         return atk.toString()
